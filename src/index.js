9d699c3d2f253622e0694313c0ff57d7318fcc94
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2021-07-05 06:44:07 -0500

    Webpack.config.js and Babel were set

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import '@styles/styles.css';
+
+const x = () => {
+   console.log('holaaa')
+}
+x()
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const htmlPlugin = require('html-webpack-plugin');
+const miniCSS = require('mini-css-extract-plugin');
+
+module.exports = {
+   entry: './src/index.js',
+   output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'main[contenthash].js',
+      clean: true,
+   },
+   resolve: {
+      extensions: ['.js'],
+      alias: {
+         '@styles': path.resolve(__dirname, 'src/styles'),
+         '@pages': path.resolve(__dirname, 'src/pages'),
+         '@routes': path.resolve(__dirname, 'src/routes'),
+         '@utils': path.resolve(__dirname, 'src/utils'),
+      }
+   },
+   module: {
+      rules: [
+         {
+            test: /\.m?js$/,
+            use: {
+               loader: 'babel-loader'
+            },
+            exclude: /node_modules/,
+         },
+         {
+            test: /\.css$/,
+            use: [
+               miniCSS.loader,
+               'css-loader',
+            ]
+         },
+      ]
+   },
+   plugins: [
+      new htmlPlugin({
+         inject: true,
+         template: './public/index.html',
+         filename: 'index.html',
+      }),
+      new miniCSS({
+         filename: './assets/styles/main[contenthash].css',
+      }),
+   ],
+   devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      historyApiFallback: true,
+      port: 3000,
+      open: true,
+      compress: true,
+   }
+}
