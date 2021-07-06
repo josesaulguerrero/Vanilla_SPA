f35110ae168a2e4745eb8f8ba61ae4c90430bac7
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2021-07-06 11:33:38 -0500

    routes were repaired, character was not being imported correctly

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -4,17 +4,19 @@ const Character = async () => {
    const id = location.hash.replace(/#(\d{1,3})/, "$1");
    const character = await GetData(id);
    const view = `
-      <button id="backButton" class="backButton">bacgdfk</button>
-      <article class="character">
-         <section class="character-img">
-            <img src="${character.image}" alt="${character.name}"/>
-         </section>
-         <section class="character-info">
-            <h2>${character.name}</h2>
-            <h3>Status: ${character.status}</h3>
-            <h3>gender: ${character.gender}</h3>
-            <h3>Origin place: ${character.origin.name}</h3>
-         </section>
+      <article class="more-info">
+         <button id="backButton" class="backButton">bacgdfk</button>
+         <article class="character">
+            <section class="character-img">
+               <img src="${character.image}" alt="${character.name}"/>
+            </section>
+            <section class="character-info">
+               <h2>${character.name}</h2>
+               <h3>Status: ${character.status}</h3>
+               <h3>gender: ${character.gender}</h3>
+               <h3>Origin place: ${character.origin.name}</h3>
+            </section>
+         </article>
       </article>
    `;
    return view;
