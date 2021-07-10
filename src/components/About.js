31dd58136034286292b21351dab79deb54c8211c
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2021-07-10 17:26:31 -0500

    about was updated

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,8 @@ export function About() {
    $about.id = 'about';
    $about.classList.add('about');
    $about.innerHTML = `
-      <h2>About</h2>
+      <h2>About this project</h2>
+      <p>This website is a simple practice, whose only purpose is to practise my skills on JS</p>
    `;
 
    return $about;
